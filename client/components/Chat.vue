0e187adafbef3c7661234be3cde696f9b65d1ff7
Pavel Djundik <xPaw@users.noreply.github.com>
2019-11-04 11:21:05 +0200

    Mentions

--- client/components/Chat.vue
+++ client/components/Chat.vue
@@ -37,6 +37,11 @@
 							:text="channel.topic"
 					/></span>
 					<button
+						class="mentions"
+						aria-label="Open your mentions"
+						@click="openMentions"
+					/>
+					<button
 						class="menu"
 						aria-label="Open the context menu"
 						@click="openContextMenu"
@@ -185,6 +190,11 @@ export default {
 				event: event,
 				channel: this.channel,
 				network: this.network,
+			});
+		},
+		openMentions() {
+			this.$root.$emit("mentions:toggle", {
+				event: event,
 			});
 		},
 	},
